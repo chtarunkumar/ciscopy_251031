12ce083d39667d0d346acc993cf0dd6f76628091
Tarun kumar <chenchali.cse.20063@iiitbh.ac.in>
2025-11-06 09:46:15 +0530

    mail

---
+++ day5/config.py
@@ -0,0 +1,7 @@
+# Your Gmail address and App Password
+# Sender address
+from_address = "tarunkumarchanchali@gmail.com"
+# Generate in Google Account -> Security -> App passwords (Two-Step Verification)
+app_password = "hgvkeusintnddlpl"  
+# Receiver address
+to_address = "gmaheswaranmca@gmail.com"
